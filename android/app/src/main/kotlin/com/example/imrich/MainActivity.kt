919d34ea5c3636b554a8b77dda2bd74a21ee8887
iamhemantgauhai <hemant20@navgurukul.org>
2022-01-20 18:59:18 +0530

    practising flutter

---
+++ android/app/src/main/kotlin/com/example/imrich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.imrich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart' show AppBar, AssetImage, Center, Colors, Image, MaterialApp, Scaffold, Text, runApp;
+
+//The main function is the starting point for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Center(child: Text("I Am Rich")),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
